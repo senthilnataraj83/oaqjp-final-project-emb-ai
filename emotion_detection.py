d15688f7509d3d9a9f8ce7abdb4beaefeca85548
senthilnataraj83 <senthil.nataraj83@gmail.com>
2026-06-21 21:21:55 -0400

    Update emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -2,17 +2,32 @@ import requests
 import json
 
 def emotion_detector(text_to_analyze):
-    # URL for the Watson NLP Emotion Predict service
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    
-    # Required headers for the Watson NLP API
     headers = {"grpc-metadata-mm-model-id": "emotion_sn_watson_nlp_expr"}
-    
-    # Input JSON payload containing the text to analyze
     myobj = { "raw_document": { "text": text_to_analyze } }
     
-    # Sending a POST request to the API
     response = requests.post(url, json = myobj, headers = headers)
     
-    # Return the raw text of the response
-    return response.text
+    # Check for client side error or blank text inputs
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+        
+    formatted_response = json.loads(response.text)
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    dominant_emotion = max(emotions, key=emotions.get)
+    
+    return {
+        'anger': emotions['anger'],
+        'disgust': emotions['disgust'],
+        'fear': emotions['fear'],
+        'joy': emotions['joy'],
+        'sadness': emotions['sadness'],
+        'dominant_emotion': dominant_emotion
+    }
